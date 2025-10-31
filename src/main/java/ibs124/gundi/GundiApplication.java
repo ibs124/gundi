1c64687a5077f8704704c517de5113113ecc6146
Ivo <ibs124official@gmail.com>
2025-10-31 10:30:21 +0200

    Set initial project structure

---
+++ src/main/java/ibs124/gundi/GundiApplication.java
@@ -0,0 +1,13 @@
+package ibs124.gundi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GundiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GundiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ibs124/gundi/GundiApplicationTests.java
@@ -0,0 +1,13 @@
+package ibs124.gundi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GundiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
