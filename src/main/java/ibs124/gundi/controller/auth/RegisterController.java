fa84a8ef3486edfef3bd6bc5fc230ee58a08ab1d
Ivo <ibs124official@gmail.com>
2025-11-09 07:17:00 +0200

    Add register link @fragments/sidebar

--- src/main/java/ibs124/gundi/controller/auth/RegisterController.java
+++ src/main/java/ibs124/gundi/controller/auth/RegisterController.java
@@ -1,8 +1,17 @@
 package ibs124.gundi.controller.auth;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import ibs124.gundi.constant.Routes;
+import ibs124.gundi.constant.Templates;
 
 @Controller
 public class RegisterController {
+
+    @GetMapping(Routes.REGISTER)
+    public String getMethodName() {
+        return Templates.REGISTER;
+    }
 
 }
