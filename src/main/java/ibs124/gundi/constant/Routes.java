8a244a9397363a6126da2e001fc8622bcc89c3c1
Ivo <ibs124official@gmail.com>
2025-11-09 15:33:58 +0200

    Add core route constants

--- src/main/java/ibs124/gundi/constant/Routes.java
+++ src/main/java/ibs124/gundi/constant/Routes.java
@@ -2,6 +2,11 @@ package ibs124.gundi.constant;
 
 public abstract class Routes {
 
+    public static final String CREATE = "/create";
+    public static final String UPDATE = "/update";
+    public static final String ERROR = "/error";
+    public static final String SUCCESS = "/success";
+
     public static final String INDEX = "/";
 
     private static final String AUTH = "/auth";
