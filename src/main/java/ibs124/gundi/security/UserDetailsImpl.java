98f985f226ed7fe97a3387c33fcbc032ded366f2
Ivo <ibs124official@gmail.com>
2025-11-13 14:58:03 +0200

    Add  /security/UserDetailsImpl

---
+++ src/main/java/ibs124/gundi/security/UserDetailsImpl.java
@@ -0,0 +1,78 @@
+package ibs124.gundi.security;
+
+import java.time.Instant;
+import java.util.Collection;
+
+import org.springframework.security.core.CredentialsContainer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserDetailsImpl implements UserDetails, CredentialsContainer {
+
+    private Collection<? extends GrantedAuthority> authorities;
+    private String username;
+    private String passowrd;
+    private String email;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    @Override
+    public void eraseCredentials() {
+        this.setPassowrd(null);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        this.authorities = authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.passowrd;
+    }
+
+    public String getPassowrd() {
+        return passowrd;
+    }
+
+    public void setPassowrd(String passowrd) {
+        this.passowrd = passowrd;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
