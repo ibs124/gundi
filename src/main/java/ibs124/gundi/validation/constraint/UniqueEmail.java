275bdadb59b436fe8d4d0261fe64cb1e2628f282
Ivo <ibs124official@gmail.com>
2025-11-15 15:52:48 +0200

    Add validation/validator/UniqueEmailValidator

---
+++ src/main/java/ibs124/gundi/validation/constraint/UniqueEmail.java
@@ -0,0 +1,26 @@
+package ibs124.gundi.validation.constraint;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import ibs124.gundi.config.message.ErrorMessageConfig;
+import ibs124.gundi.validation.validator.UniqueEmailValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+@Target({ ElementType.FIELD, ElementType.METHOD, ElementType.ANNOTATION_TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Constraint(validatedBy = { UniqueEmailValidator.class })
+public @interface UniqueEmail {
+
+    String message() default ErrorMessageConfig.USERNAME_TAKEN;
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
