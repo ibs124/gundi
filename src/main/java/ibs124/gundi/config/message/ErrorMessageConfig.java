95188eff929dd0e28eec3580afc2112ad22e1ffe
Ivo <ibs124official@gmail.com>
2025-11-15 15:39:37 +0200

    Add validation/constraint/@UniqueUsername

--- src/main/java/ibs124/gundi/config/message/ErrorMessageConfig.java
+++ src/main/java/ibs124/gundi/config/message/ErrorMessageConfig.java
@@ -11,7 +11,9 @@ public abstract class ErrorMessageConfig {
     public static final String USERNAME = """
             Username must be between 3 and 20 characters long,
             it can only use letters, numbers and underscores.
-            """;;
+            """;
+
+    public static final String USERNAME_TAKEN = "Username already taken.";
 
     public static final String FULL_NAME_ERROR = "";
 }
