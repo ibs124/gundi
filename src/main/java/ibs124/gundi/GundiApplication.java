faba9d0242c3f49bffe19ce95f3205178dfa8bf7
Ivo <ibs124official@gmail.com>
2026-01-20 12:54:05 +0200

    Configure springframework auditing

--- src/main/java/ibs124/gundi/GundiApplication.java
+++ src/main/java/ibs124/gundi/GundiApplication.java
@@ -3,11 +3,13 @@ package ibs124.gundi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import ibs124.gundi.configuration.PropertyConfig;
 
 @SpringBootApplication
 @EnableConfigurationProperties(PropertyConfig.class)
+@EnableJpaAuditing
 public class GundiApplication {
 
     public static void main(String[] args) {
