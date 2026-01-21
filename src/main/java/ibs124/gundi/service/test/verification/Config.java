f3ab9945880b9ff82ff57a4572b1207e8f3a9757
Ivo <ibs124official@gmail.com>
2026-01-21 17:44:11 +0200

    Add basic email sending and template support

--- src/main/java/ibs124/gundi/service/test/verification/Config.java
+++ src/main/java/ibs124/gundi/service/test/verification/Config.java
@@ -2,16 +2,22 @@ package ibs124.gundi.service.test.verification;
 
 abstract class Config {
 
-    public static final String EMAIL_TO = "ibs124experimental@gmail.com";
+    public static final String KEY_CSS_INLINE = "inlineCss";
+
+    public static final String KEY_VERIFICATION_TOKEN = "verificationToken";
 
     public static final String EMAIL_FROM = "noreply@gundi.com";
 
-    public static final String EMAIL_DISPLAY_NAME = "Gundi security team";
+    public static final String EMAIL_DISPLAY_NAME = "Gundi";
 
-    public static final String VERIFICATION_CODE = "123456";
+    public static final String EMAIL_TO = "ibs124experimental@gmail.com";
 
-    public static final String HTML_PATH = "/emails/new-user-verification";
+    public static final String EMAIL_SUBJECT = "Confirm your email address";
 
-    public static final String CSS_PATH = "/css/emails";
+    public static final String VERIFICATION_CODE = "094532";
+
+    public static final String PATH_HTML = "/emails/new-user-verification";
+
+    public static final String PATH_CSS = "classpath:static//css/emails.css";
 
 }
--- src/main/java/ibs124/gundi/service/test/verification/VerificationEmailSender.java
+++ src/main/java/ibs124/gundi/service/test/verification/VerificationEmailSender.java
@@ -1,11 +1,65 @@
 package ibs124.gundi.service.test.verification;
 
+import static ibs124.gundi.service.test.verification.Config.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+
+import ibs124.gundi.model.application.EmailSendDto;
+import ibs124.gundi.model.application.TemplateCompileDto;
+import ibs124.gundi.service.utility.EmailSendingService;
+import ibs124.gundi.service.utility.TemplateCompileService;
 
 @Component
 public class VerificationEmailSender {
 
+    private final EmailSendingService emailSendingService;
+    private final TemplateCompileService templateCompileService;
+    private final Resource cssResource;
+
+    public VerificationEmailSender(
+            EmailSendingService emailSendingService,
+            TemplateCompileService templateCompileService,
+            @Value(PATH_CSS) Resource cssResource) {
+        this.emailSendingService = emailSendingService;
+        this.templateCompileService = templateCompileService;
+        this.cssResource = cssResource;
+    }
+
     public void run() {
+        String css = this.loadCss();
+
+        TemplateCompileDto templateRequest = new TemplateCompileDto(
+                PATH_HTML, null);
+
+        templateRequest.addAttributre(KEY_CSS_INLINE, css);
+        templateRequest.addAttributre(KEY_VERIFICATION_TOKEN, VERIFICATION_CODE);
+
+        String htmlMessage = this.templateCompileService.compileHtml(templateRequest);
+
+        EmailSendDto emailRequest = new EmailSendDto(
+                EMAIL_FROM,
+                EMAIL_DISPLAY_NAME,
+                EMAIL_TO,
+                EMAIL_SUBJECT,
+                htmlMessage,
+                true);
+
+        this.emailSendingService.sendEmail(emailRequest);
+
+    }
+
+    private String loadCss() {
+        try (InputStream is = this.cssResource.getInputStream()) {
+            return new String(is.readAllBytes(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
 
     }
 
