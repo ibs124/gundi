a0b3c0c85c71936b9769c47a613325e4b325c2a2
Ivo <ibs124official@gmail.com>
2026-03-06 12:02:21 +0200

    Fix AbstractTokenRepository

--- src/main/java/ibs124/gundi/repository/AbstractTokenRepository.java
+++ src/main/java/ibs124/gundi/repository/AbstractTokenRepository.java
@@ -14,7 +14,7 @@ import java.time.Instant;
 public interface AbstractTokenRepository<T extends AbstractToken>
         extends JpaRepository<T, Long> {
 
-    Optional<@Valid T> findBySecretAndExpiresAtBefore(String value, Instant expiresAt);
+    Optional<@Valid T> findBySecretAndExpiresAtAfter(String value, Instant expiresAt);
 
     boolean existsBySecret(String value);
 
