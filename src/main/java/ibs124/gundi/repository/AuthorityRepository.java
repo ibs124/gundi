58c8d3501dd04b00c90a339d6c1a1e722af60f06
Ivo <ibs124official@gmail.com>
2026-03-19 13:07:42 +0200

    Add findByNameIn in AuthorityRepository

--- src/main/java/ibs124/gundi/repository/AuthorityRepository.java
+++ src/main/java/ibs124/gundi/repository/AuthorityRepository.java
@@ -1,5 +1,8 @@
 package ibs124.gundi.repository;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,7 @@ import ibs124.gundi.model.persistence.AuthorityEntity;
 
 @Repository
 public interface AuthorityRepository extends JpaRepository<AuthorityEntity, Long> {
+
+    List<AuthorityEntity> findByNameIn(Collection<String> names);
 
 }
