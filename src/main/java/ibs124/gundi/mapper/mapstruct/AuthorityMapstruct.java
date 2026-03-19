27d9499ba490fd607bf043a29d65a9c18122e112
Ivo <ibs124official@gmail.com>
2026-03-19 13:58:23 +0200

    Fix Authority mapping

--- src/main/java/ibs124/gundi/mapper/mapstruct/AuthorityMapstruct.java
+++ src/main/java/ibs124/gundi/mapper/mapstruct/AuthorityMapstruct.java
@@ -1,11 +1,18 @@
 package ibs124.gundi.mapper.mapstruct;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import ibs124.gundi.constant.Mappers;
 import ibs124.gundi.mapper.AuthorityMapper;
+import ibs124.gundi.model.persistence.AuthorityEntity;
 
 @Mapper(componentModel = Mappers.COMPONENT_MODEL)
 public interface AuthorityMapstruct extends AuthorityMapper {
+
+    @Mapping(source = "name", target = "authority")
+    @Override
+    SimpleGrantedAuthority mapToSecurityModel(AuthorityEntity src);
 
 }
