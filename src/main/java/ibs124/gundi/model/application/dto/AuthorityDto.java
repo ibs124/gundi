e480ffac4315ae47b869ed6312be61e326cf2f2f
Ivo <ibs124official@gmail.com>
2026-03-19 11:22:24 +0200

    Implement Authority in AuthorityDto

--- src/main/java/ibs124/gundi/model/application/dto/AuthorityDto.java
+++ src/main/java/ibs124/gundi/model/application/dto/AuthorityDto.java
@@ -1,10 +1,18 @@
 package ibs124.gundi.model.application.dto;
 
+import ibs124.gundi.model.application.Authority;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 
 @NotNull
-public record AuthorityDto(@PositiveOrZero long id, @NotBlank String name) {
+public record AuthorityDto(
+        @PositiveOrZero long id,
+        @NotBlank String name) implements Authority {
+
+    @Override
+    public String getAuthority() {
+        return this.name();
+    }
 
 }
