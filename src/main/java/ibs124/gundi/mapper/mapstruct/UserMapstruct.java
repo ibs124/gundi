65b4ec68c96e8538c674466e73a23b3c142ea7de
Ivo <ibs124official@gmail.com>
2026-03-20 10:20:44 +0200

    Fix UserMapstruct

--- src/main/java/ibs124/gundi/mapper/mapstruct/UserMapstruct.java
+++ src/main/java/ibs124/gundi/mapper/mapstruct/UserMapstruct.java
@@ -7,6 +7,8 @@ import ibs124.gundi.mapper.UserMapper;
 import ibs124.gundi.constant.Mappers;
 import ibs124.gundi.mapper.AuthorityMapper;
 import ibs124.gundi.model.application.dto.UserCreateDto;
+import ibs124.gundi.model.application.dto.UserDto;
+import ibs124.gundi.model.persistence.UserEntity;
 import ibs124.gundi.model.presentation.RegisterRequest;
 
 @Mapper(componentModel = Mappers.COMPONENT_MODEL, uses = AuthorityMapper.class)
@@ -16,4 +18,7 @@ interface UserMapstruct extends UserMapper {
     @Mapping(source = Mappers.EMAIL, target = Mappers.PRIMARY_EMAIL)
     UserCreateDto mapToApplicationModel(RegisterRequest src);
 
+    @Mapping(source = "enabled", target = "isEnabled")
+    @Override
+    UserDto mapToApplicationModel(UserEntity src);
 }
